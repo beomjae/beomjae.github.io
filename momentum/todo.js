b785d350bd4d27486d0b83cf5b7c21e9e8f0b314
Beomjae Lee <beomjae@gmail.com>
2019-01-06 21:22:53 +0900

    add momentum

---
+++ momentum/todo.js
@@ -0,0 +1,71 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+let toDos = [];
+
+function deleteToDo(event) {
+    console.log(event.target.parentNode);
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    console.log(cleanToDos);
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+    
+function loadToDos() {
+    const loadedTodos = localStorage.getItem(TODOS_LS);
+    if(loadedTodos !== null) {
+        const parsedTodos = JSON.parse(loadedTodos);
+        parsedTodos.forEach(function(toDo){
+            paintTodo(toDo.text);
+        })
+    } 
+}
+
+function paintTodo(text) {
+    console.log(text);
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerHTML = "X";
+    delBtn.addEventListener("click", deleteToDo);
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+    toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintTodo(currentValue);
+    toDoInput.value = "";
+}
+
+function toDoinit() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+toDoinit(); 
